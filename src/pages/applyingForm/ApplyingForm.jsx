06cb76db9de1e7c204da76af04498f5e038c0913
ahmedeid533 <ahmedeidgoma333@gmail.com>
2026-02-01 12:52:08 +0200

    fix: use unpkg CDN for PDF.js worker (cdnjs missing v5.4.530)

--- src/pages/applyingForm/ApplyingForm.jsx
+++ src/pages/applyingForm/ApplyingForm.jsx
@@ -244,7 +244,7 @@ import { nanoid } from "nanoid";
 import * as pdfjsLib from 'pdfjs-dist';
 
 // Configure PDF.js worker using CDN (fixes production bundle issues)
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.530/pdf.worker.min.mjs';
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://unpkg.com/pdfjs-dist@5.4.530/build/pdf.worker.min.mjs';
 
 /**
  * Updated ApplyingForm.jsx
